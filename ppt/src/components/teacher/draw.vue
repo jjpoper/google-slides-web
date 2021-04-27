49e809561adc82ac334acaaea196a0171f203446
kerwinliu <991920040@qq.com>
2021-04-27 21:14:56 +0800

    update draw

--- ppt/src/components/teacher/draw.vue
+++ ppt/src/components/teacher/draw.vue
@@ -1,31 +1,46 @@
 <template>
-  <div id="canvasouter" >
-    <img :src="imgUrl" v-if="imgUrl"/>
-  </div>
+  <div>
+    <div id="canvasouter" v-for="item in imgUrls" :key="item.user_id">
+      <img :src="item.content" />
+    </div>
+  </div>  
 </template>
 
 <script>
+import { getCurrentPageAnswerList } from '@/model/store.teacher'
 
 export default {
     props: {
-      sendCanvas: {type: Function }
+      sendCanvas: {type: Function },
+      data: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      }
     },
     data() {
       return {
-        imgUrl: ''
+        imgUrls: []
       }
     },
     mounted() {
+      this.imgUrls = getCurrentPageAnswerList(this.data.page_id, this.data.items[0].type)
       EventBus.$on('draw', (data)=>{
         // 通知展示当前pageid，当前itemid的评论框
         this.draw(data)
       })
     },
     methods: {
-      draw(base64Url) {
+      draw({user_id, content}) {
         // console.log(data)
         // const cxt = this.cxt
-        this.imgUrl = base64Url
+        const index = this.imgUrls.findIndex((item) => item.user_id === user_id)
+        if(index > -1) {
+          this.imgUrls[index].content = content
+        } else {
+          this.imgUrls.push({user_id, content})
+        }
 
         // let myCanvas = document.getElementById("canvas");
         // const img = new Image();
