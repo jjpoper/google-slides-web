ba4eeccbfbdae02cc1e04a493816e6e478567267
kerwinliu <991920040@qq.com>
2021-06-10 17:24:12 +0800

    增加老师端展示按钮

--- ppt/src/components/teacher/teacherControlPanel.vue
+++ ppt/src/components/teacher/teacherControlPanel.vue
@@ -65,7 +65,20 @@
 
       <strong>{{ isClosed ? "Closed" : current_model }}</strong>
     </div>
-
+    <el-tooltip content="show comment" placement="top">
+      <div
+        class="readchat comment"
+        @click="showStudentQuestions"
+      >
+        <el-switch
+          style="display: block"
+          v-model="questionModalVisiable"
+          active-color="#13ce66"
+          inactive-color="#999"
+          active-text="students comment">
+        </el-switch>
+      </div>
+    </el-tooltip>
     <div
       :class="isResponseShow ? 'button_area back_red' : 'button_area'"
       @click="showRes()"
@@ -466,5 +479,18 @@ strong {
 .button_text {
   line-height: 20px;
   font-size: 15px;
+}
+.readchat {
+  font-size: 30px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+}
+.comment{
+  background: #fff; height: 40px; padding: 0 5px;
+  margin-top: 5px;
+  border-radius: 4px;
+
 }
 </style>
