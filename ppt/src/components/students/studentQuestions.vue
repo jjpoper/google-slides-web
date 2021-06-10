70229ae5490a7520b8acdaebb78ab60b62a4b438
kerwinliu <991920040@qq.com>
2021-06-10 09:21:04 +0800

    增加学生端对ppt进行评论打点

---
+++ ppt/src/components/students/studentQuestions.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="mark-area" @click="markup($event)">
+      <div v-for="(item, index) in marks" :key="index" class="markitem" :style="`top:${item.top}px;left:${item.left}px`"></div>
+      <div class="">
+      </div>
+    </div>
+</template>
+<style scoped>
+.mark-area{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 50px;
+  background-color: transparent;
+}
+.markitem{
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: red;
+  position: absolute;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      marks: []
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    markup(e) {
+      // console.log(e)
+      const {pageX: left, pageY: top} = e
+      this.marks.push({left: left - 15, top: top - 15})
+    }
+  }
+}
+</script>
