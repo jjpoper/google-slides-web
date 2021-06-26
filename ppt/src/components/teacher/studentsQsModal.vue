6c4cb166bfa808dd695b9432d84c1b5cc0eb8dcc
kerwinliu <991920040@qq.com>
2021-06-26 13:07:05 +0800

    增加人员联动

--- ppt/src/components/teacher/studentsQsModal.vue
+++ ppt/src/components/teacher/studentsQsModal.vue
@@ -2,16 +2,7 @@
   <div style="background-color: #E9EEF3;">
     <div class="mark-area">
       <pptcontent :url="url"/>
-      <!-- <div
-        v-for="(item, index) in marks"
-        :key="index"
-        :class="`markitem ${selectedIndex === index ? 'markitemhover' : ''}`"
-        :style="`top:${item.top}px;left:${item.left}px;`"
-        @click.stop="selectMark(item, index)"
-      >
-        <div class="innermark" :style="`background-color:${item.background || 'red'}; `"/>
-      </div> -->
-      <template v-for="(item, index) in marks">
+      <template v-for="(item, index) in currentMarks">
         <div
           :key="item.id"
           v-if="item.pointType !== 'box'" 
@@ -37,7 +28,17 @@
       </template>
     </div>   
     <div class="right-area">
-      <recordCommentList :list="marks" :selectMark="selectMark" :selectedIndex="selectedIndex"/>
+      <recordCommentList :list="currentMarks" :selectMark="selectMark" :selectedIndex="selectedIndex"/>
+    </div>
+    <div class="select-users" v-if="users.length > 2">
+      <el-select v-model="userId" placeholder="ALL">
+        <el-option
+          v-for="item in users"
+          :key="item.user_id"
+          :label="item.user_name"
+          :value="item.user_id">
+        </el-option>
+      </el-select>
     </div>
   </div>
 </template>
@@ -67,7 +68,14 @@ export default {
       selectedIndex: -1,
       currentPageId: 0,
       marks: [],
+      users: [{user_id: 0, user_name: 'ALL'}],
+      userId: 0,
     };
+  },
+  computed: {
+    currentMarks() {
+      return this.marks.filter((item) => this.userId == 0 || item.user_id === this.userId)
+    }
   },
   created() {
     console.log(this.list)
@@ -77,6 +85,15 @@ export default {
         ...item.data
       }
     })
+    let i,
+      users = [{user_id: 0, user_name: 'ALL'}];
+    for(i = 0; i < this.list.length; i++) {
+      const {user_id, user_name} = this.list[i]
+      if(users.findIndex(item => item.user_id === user_id) === -1) {
+        users.push({user_id, user_name})
+      }
+    }
+    this.users = users
   },
   methods: {
     selectMark(item, index) {
@@ -132,5 +149,12 @@ export default {
   height: 20px;
   border-radius: 10px;
   margin: 3px;
+}
+.select-users{
+  position: fixed;
+  top: 5px;
+  right: 30%;
+  width: 100px;
+  height: 40px;
 }
 </style>
