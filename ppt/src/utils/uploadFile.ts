ac6185dcd9be2b8b70c001f60d6952bc03814c7d
kerwinliu <991920040@qq.com>
2021-10-20 00:59:22 +0800

    media题型替换上传控件

--- ppt/src/utils/uploadFile.ts
+++ ppt/src/utils/uploadFile.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { initializeApp } from 'firebase/app';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
@@ -18,54 +19,59 @@ const storage = getStorage();
 // const storage = firebase.storage();
 
 // var storageRef = storage.ref();
-var LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif?a';
+// var LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif?a';
+type OnProgress = any
 
-export const upFireBaseFile = async (file) => {
-  const now = Date.now()
-  console.log(now, '计时 开始上传')
-  const metadata = {
-    contentType: 'video/mp4'
-  };
-  const storageRef = ref(storage, 'mp4/' + file.name);
-  const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+export const upFireBaseFile = async (file: File, onProgress: OnProgress) => {
+  return new Promise((res, rej) => {
+    const now = Date.now()
+    console.log(now, '计时 开始上传')
+    const metadata = {
+      contentType: file.type
+    };
+    const storageRef = ref(storage, `file/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
 
-  // Listen for state changes, errors, and completion of the upload.
-  uploadTask.on('state_changed',
-    (snapshot) => {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case 'paused':
-          console.log('Upload is paused');
-          break;
-        case 'running':
-          console.log('Upload is running');
-          break;
-      }
-    }, 
-    (error) => {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case 'storage/unauthorized':
-          // User doesn't have permission to access the object
-          break;
-        case 'storage/canceled':
-          // User canceled the upload
-          break;
-
-        case 'storage/unknown':
-          // Unknown error occurred, inspect error.serverResponse
-          break;
-      }
-    }, 
-    () => {
-      // Upload completed successfully, now we can get the download URL
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        console.log('File available at', downloadURL);
-        console.log(Date.now() - now, '计时 上传成功')
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(`Upload is ${progress}`);
+        // eslint-disable-next-line no-unused-expressions
+        onProgress && onProgress(progress)
+        // switch (snapshot.state) {
+        //   case 'paused':
+        //     console.log('Upload is paused');
+        //     break;
+        //   case 'running':
+        //     console.log('Upload is running');
+        //     break;
+        //   default
+        // }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        // switch (error.code) {
+        //   case 'storage/unauthorized':
+        //     // User doesn't have permission to access the object
+        //     break;
+        //   case 'storage/canceled':
+        //     // User canceled the upload
+        //     break;
+        //   case 'storage/unknown':
+        //     // Unknown error occurred, inspect error.serverResponse
+        //     break;
+        // }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+          console.log(Date.now() - now, '计时 上传成功')
+          res(downloadURL)
+        });
       });
-    }
-  );
+  })
 }
