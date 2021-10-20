1063deea68668617ded148246ce10ecdd198f2f2
kerwinliu <991920040@qq.com>
2021-10-20 23:08:20 +0800

    优化lodaing

--- ppt/src/components/common/commonUpload.vue
+++ ppt/src/components/common/commonUpload.vue
@@ -7,11 +7,14 @@
       @change="onUpload"/>
       <el-dialog
         v-if="progress > 0"
-        :title="`uploading ${progress}%`"
+        title="uploading"
         visible
         :append-to-body="true"
         :show-close="false"
       >
+        <div class="progress">
+          <span :style="`width: ${progress}%;`"><span>{{progress}}%</span></span>
+        </div>
       </el-dialog>
   </div>
 </template>
@@ -40,12 +43,15 @@ export default {
       console.log(file)
       this.progress = 0
       upFireBaseFile(file, this.onProgressUpLoad).then((result) => {
-        this.progress = 0
+        this.progress = 100
         this.onSuccess(file, result)
+        setTimeout(() => {
+          this.progress = 0
+        }, 100);
       })
     },
     onProgressUpLoad(progress) {
-      this.progress = parseInt(progress)
+      this.progress = Math.min(parseInt(progress), 99)
     }
   }
 }
@@ -59,5 +65,42 @@ export default {
   left: 0;
   opacity: 0;
   cursor: pointer;
+}
+.progress {
+  height: 20px;
+  background: #ebebeb;
+  border-left: 1px solid transparent;
+  border-right: 1px solid transparent;
+  border-radius: 10px;
+}
+.progress > span {
+  position: relative;
+  float: left;
+  margin: 0 -1px;
+  min-width: 30px;
+  height: 18px;
+  line-height: 16px;
+  text-align: right;
+  border: 1px solid;
+  border-radius: 10px;
+  background: #85c440;
+  border-color: #78b337 #6ba031 #568128;
+  background-image: -webkit-linear-gradient(top, #b7dc8e 0%, #99ce5f 70%, #85c440 100%);
+  background-image: -moz-linear-gradient(top, #b7dc8e 0%, #99ce5f 70%, #85c440 100%);
+  background-image: -o-linear-gradient(top, #b7dc8e 0%, #99ce5f 70%, #85c440 100%);
+  background-image: linear-gradient(to bottom, #b7dc8e 0%, #99ce5f 70%, #85c440 100%);
+  -webkit-box-shadow: inset 0 1px rgba(255, 255, 255, 0.3), 0 1px 2px rgba(0, 0, 0, 0.2);
+  box-shadow: inset 0 1px rgba(255, 255, 255, 0.3), 0 1px 2px rgba(0, 0, 0, 0.2);
+}
+.progress .green {
+ 
+}
+.progress > span > span {
+  padding: 0 8px;
+  font-size: 11px;
+  font-weight: bold;
+  color: #404040;
+  color: rgba(0, 0, 0, 0.7);
+  text-shadow: 0 1px rgba(255, 255, 255, 0.4);
 }
 </style>
