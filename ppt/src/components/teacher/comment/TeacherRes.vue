2a2bfe7c0c96b46e6cca0b5e70379cc2725c44e5
kerwinliu <991920040@qq.com>
2021-11-21 00:09:26 +0800

    update pad 样式

--- ppt/src/components/teacher/comment/TeacherRes.vue
+++ ppt/src/components/teacher/comment/TeacherRes.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="left-answer shadow" v-if="item.title">
-    <div class="answerdetail">
-      <div class="answer-media flex-center" v-if="item.title.indexOf('data:image/') > -1">
+  <div class="tfleft-answer tfshadow" v-if="item.title">
+    <div class="tfanswerdetail">
+      <div class="tfanswer-media flex-center" v-if="item.title.indexOf('data:image/') > -1">
         <div class="inner-media" >
           <base64image :url="item.title" />
         </div>
       </div>
-      <div class="answer-media flex-center" v-else-if="item.title.indexOf('.mp3') > -1">
-        <div class="inner-media" >
+      <div class="tfanswer-media flex-center" v-else-if="item.title.indexOf('.mp3') > -1">
+        <div class="tfinner-media" >
           <audio-player :url="item.title"/>
         </div>
         <!-- <div class="transformmask"></div> -->
       </div>
-      <div class="answer-media flex-center"  v-else-if="item.title.indexOf('.webm') > -1">
-        <div class="inner-media">
+      <div class="tfanswer-media flex-center"  v-else-if="item.title.indexOf('.webm') > -1">
+        <div class="tfinner-media">
           <video  preload="meta" controls="false" :src="item.title" style="width:100%;" />
         </div>
         <!-- <div class="transformmask"></div> -->
       </div>
-      <div class="answer-text" v-else-if="item.title.indexOf('[') > -1">
+      <div class="tfanswer-text" v-else-if="item.title.indexOf('[') > -1">
         <div v-for="(text, index) in getAnswer(item.title)" :key="index">{{ text }}</div>
       </div>
-      <div class="answer-text" v-else>
+      <div class="tfanswer-text" v-else>
         {{item.title}}
       </div>
     </div>
-    <div class="user">
-      <div class="usericon">{{userData.sname.split("")[0]}}</div>
-      <div class="info">
-        <p class="uname">{{userData.sname}}</p>
-        <p class="utime">{{getTimeText(item.answertime)}}</p>
+    <div class="tfuser">
+      <div class="tfusericon">{{userData.sname.split("")[0]}}</div>
+      <div class="tfinfo">
+        <p class="tfuname">{{userData.sname}}</p>
+        <p class="tfutime">{{getTimeText(item.answertime)}}</p>
       </div>
     </div>
     <slot />
@@ -75,88 +75,5 @@ export default {
 </script>
 <style scoped>
 *{box-sizing: border-box;}
-.shadow{
-  box-shadow: 0px 1px 3px 3px rgb(0 0 0 / 16%);
-  border-radius: 8px;
-}
-.left-answer {
-  width: 402px;
-  background-color: #fff;
-  padding: 16px 21px 30px 16px;
-  box-sizing: border-box;
-  margin-bottom: 20px;
-  /* border: 1px solid #999; */
-}
-.answer-media {
-  width: 360px;
-  height: 180px;
-  border-radius: 10px;
-  background-color: rgba(228,228,228, 0.3);
-  position: relative;
-  /* padding: 2px; */
-}
-.inner-media{
-  width: 100%;
-  height: 100%;
-  position: relative;
-  border-radius: 8px;
-  overflow: hidden;
-}
-.answer-text{
-  flex: 1;
-  max-height: 180px;
-  min-height: 50px;
-  display: flex;
-  box-sizing: border-box;
-  font-size: 14px;
-  font-family: Inter-Bold;
-  line-height: 24px;
-  color: #000000;
-  overflow-y: scroll;
-}
-.user{
-  width: 100%;
-  height: 52px;
-  display: flex;
-  flex-direction: row;
-  margin-top: 20px;
-}
-.usericon{
-  width: 52px;
-  height: 52px;
-  border-radius: 26px;
-  background-color: #15C39A;
-  text-align: center;
-  line-height: 52px;
-  font-size: 28px;
-  font-family: Inter-Bold;
-  margin-right: 10px;
-}
-.info{
-  height: 52px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-}
-.uname{
-  font-size: 18px;
-  font-family: Inter-Bold;
-  line-height: 24px;
-  color: #15C39A;
-  opacity: 1;
-}
-.utime{
-  font-size: 14px;
-  font-family: Inter-Bold;
-  line-height: 24px;
-  color: #000000;
-  opacity: 1;
-}
-.nocontrols::-webkit-media-controls{ 
-  display:none !important;
-}
-video{
-  width: 100%; height: 100%; object-fit: cover
-}
+
 </style>
--- ppt/src/components/teacher/commentModal.vue
+++ ppt/src/components/teacher/commentModal.vue
@@ -1,78 +1,62 @@
 <template>
-  <div class="fixModal" v-if="modalVisiable">
-    <div class="commentModal">
-      <div class="header">
-        <p class="title">Feedback for {{commentData.sname}}</p>
+  <div class="tffixModal" v-if="modalVisiable">
+    <div class="tfcommentModal">
+      <div class="tfheader">
+        <p class="tftitle">Feedback for {{commentData.sname}}</p>
         <img
           src="../../assets/picture/closecom.png"
-          class="close" 
+          class="tfclose" 
           @click="closeModal"
         />
       </div>
-      <div class="mbox" v-if="commentData.pageId">
-        <div class="left">
+      <div class="tfmbox" v-if="commentData.pageId">
+        <div class="tfleft">
           <teacher-res :item="commentData" :userData="commentData" />
-          <div v-if="(commentList && commentList.length > 0) && !addmore" class="footer-button-more"
+          <div v-if="(commentList && commentList.length > 0) && !addmore" class="tffooter-button-more"
             @click="addmoreFeed">
-            <p class="more-feed">Add More Feedback</p>
-            <img src="../../assets/picture/send.png" class="more-icon"/>
+            <p class="tfmore-feed">Add More Feedback</p>
+            <img src="../../assets/picture/send.png" class="tfmore-icon"/>
           </div>
-          <div class="edit-area shadow" v-else>
-            <div class="textarea-box">
+          <div class="tfedit-area tfshadow" v-else>
+            <div class="tftextarea-box">
               <textarea
-                class="textarea"
+                class="tftextarea"
                 v-model="commentValue"
                 placeholder=""
                 @focus="focus"
                 @blur="blur"
               ></textarea>
-              <div class="text-placeholder" v-show="showPlaceholder">
+              <div class="tftext-placeholder" v-show="showPlaceholder">
                 Lonve feedback for this response……
               </div>
             </div>
-            <div class="footer-button" @click="sendMessage">
-              <div :class="`send-button ${!showPlaceholder && 'active'}`">Send Feedback</div>
+            <div class="tffooter-button" @click="sendMessage">
+              <div :class="`tfsend-button ${!showPlaceholder && 'tfactive'}`">Send Feedback</div>
             </div>
           </div>
-          <!-- <template v-if="commentData.type === ModalEventsTypeEnum.TEXT">
-            <div>
-              <textarea
-                class="textarea"
-                v-model="commentValue"
-                placeholder="Leave a message for this response..."
-              ></textarea>
-              <el-button type="primary" style="width: 100%" @click="sendMessage">send</el-button>
-            </div>
-          </template>
-          <template v-else-if="commentData.type === ModalEventsTypeEnum.VIDEO">
-            <record-video :onSend="sendVideoOrAudio" />
-          </template>
-          <template v-else-if="commentData.type === ModalEventsTypeEnum.AUDIO">
-            <record-audio :onSend="sendVideoOrAudio" />
-          </template> -->
         </div>
-        <div class="right" v-if="commentList && commentList.length > 0">
+        <div class="tfright" v-if="commentList && commentList.length > 0">
           <teacher-res
             v-for="(item, index) in commentList"
             :item="item"
             :userData="commentData"
             :key="index.toString()">
-            <div class="feed-item feed-media" v-if="item.commentType === 'video'">
+            <div class="tffeed-item feed-media" v-if="item.commentType === 'video'">
               <video preload="meta" controls="false" :src="item.title" style="width:100%;" />
             </div>
-            <div class="feed-item feed-media feed-media-audio" v-else-if="item.commentType === 'audio'">
+            <div class="tffeed-item feed-media feed-media-audio" v-else-if="item.commentType === 'audio'">
               <audio-player :url="item.title"/>
             </div>
-            <div v-else class="feed-item feed-text">
+            <div v-else class="tffeed-item feed-text">
               {{ item.value }}
             </div>
           </teacher-res>
-          <div class="transformmask"></div>
+          <div class="tftransformmask"></div>
         </div>
-        <div v-else class="empty">
-          <img src="../../assets/picture/empty-feed.png" class="empty-img"/>
-          <p class="title">No Feedback Yet</p>
-          <p class="empty-tip">Go to add your first reply</p>
+        <div v-else class="tfempty">
+          <img src="../../assets/picture/empty-feed.png" class="tfempty-img"/>
+          <p class="tftitle">No Feedback Yet</p>
+          <p class="tfempty-tip">Go to add your first reply</p>
         </div>
       </div>
     </div>
@@ -205,250 +189,5 @@ export default {
 };
 </script>
 <style scoped>
-*{box-sizing: border-box;}
-.shadow{
-  box-shadow: 0px 1px 3px 3px rgb(0 0 0 / 16%);
-  border-radius: 8px;
-}
-.fixModal {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.3);
-  z-index: 99999999;
-}
-.commentModal {
-  width: 1000px;
-  height: 822px;
-  position: relative;
-  top: 50%;
-  left: 50%;
-  margin-left: -500px;
-  margin-top: -411px;
-  background-color: #fff;
-  display: flex;
-  flex-direction: column;
-  border-radius: 10px;
-}
-@media screen and (max-width: 1599px) {
-  .commentModal {
-    transform: scale(0.85);
-  }
-}
-.header {
-  height: 95px;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding-left: 34px;
-  padding-right: 24px;
-  border-bottom:1px solid #D8D8D8;
-  box-sizing: border-box;
-}
-.title{
-  font-size: 20px;
-  font-family: Inter-Bold;
-  color: #000000;
-  opacity: 1;
-}
-.close {
-  width: 30px;
-  height: 30px;
-  cursor: pointer;
-}
-.mbox {
-  height: 729px;
-  width: 100%;
-  text-align: left;
-  padding: 22px 55px 22px 32px;
-  display: flex;
-  justify-content: space-between;
-  box-sizing: border-box;
-}
-.textarea-box{
-  width: 360px;
-  height: 120px;
-  background: rgba(255, 255, 255, 0.1);
-  border: 1px solid #D8D8D8;
-  opacity: 1;
-  position: relative;
-  border-radius: 10px;
-  overflow: hidden;
-}
-.textarea {
-  background-color: transparent;
-  width: 360px;
-  height: 120px;
-  padding: 5px;
-  border: none;
-  box-sizing: border-box;
-  outline: none;
-  resize: none
-}
-.left{
-  padding: 3px;
-}
-.right {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  overflow-y: scroll;
-  padding: 3px;
-  box-sizing: border-box;
-  position: relative;
-}
-.rightmtitle {
-  width: 100%;
-  min-height: 70px;
-  overflow: hidden;
-  background-color: #f2f2f2;
-  border: 1px solid #999;
-  box-sizing: border-box;
-  padding: 10px;
-  word-wrap: break-word;
-  position: relative;
-}
-.rightcomment {
-  min-height: 70px;
-  background-color: #fff;
-  width: 100%;
-  border: 1px solid #999;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-}
-.nocontrols::-webkit-media-controls{ 
-  display:none !important;
-}
-video{
-  width: 100%; height: 100%; object-fit: cover
-}
-.edit-area{
-  height: 220px;
-  width: 402px;
-  box-sizing: border-box;
-  padding: 20px;
-}
-.text-placeholder{
-  position: absolute;
-  top: 50%;
-  height: 22px;
-  transform: translateY(-11px);
-  left: 20px;
-  font-size: 14px;
-  font-family: Inter-Bold;
-  line-height: 24px;
-  color: #828282;
-  padding-right: 30px;
-  text-align: right;
-  background-image: url(../../assets/picture/pencial.png);
-  background-size: 22px 22px;
-  background-position: bottom 0 right 0;
-  background-repeat: no-repeat;
-  pointer-events: none;
-}
-.footer-button{
-  margin-top: 19px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-}
-.footer-button-more{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #15C39A;
-  box-shadow: 0px 6px 10px 0px rgb(21 195 154 / 30%);
-  border-radius: 30px;
-  width: 402px;
-  height: 60px;
-  cursor: pointer;
-}
-.send-button{
-  width: 180px;
-  height: 40px;
-  border-radius: 20px;
-  box-sizing: border-box;
-  background-color: #F6F7F7;
-  border: 1px solid #E5E5E5;
-  line-height: 40px;
-  font-size: 14px;
-  font-family: Inter-Bold;
-  color: #000000;
-  padding-left: 27px;
-  background-image: url(../../assets/picture/send-disable.png);
-  background-size: 22px 18.33px;
-  background-position: bottom 9px right 20px;
-  background-repeat: no-repeat;
-  cursor: pointer;
-}
-.send-button.active{
-  background: #15C39A;
-  color: #FFFFFF;
-  background-image: url(../../assets/picture/send.png);
-  background-size: 22px 18.33px;
-  background-position: bottom 9px right 20px;
-  background-repeat: no-repeat;
-}
-.feed-item{
-  width: 360px;
-  height: 100px;
-  background: rgba(228,228,228, 0.2);
-  border-radius: 8px;
-  margin-top: 17px;
-  box-sizing: border-box;
-  position: relative;
-  overflow-y: scroll;
-}
-.feed-text{
-  padding: 15px 22px;
-  font-size: 14px;
-  font-family: Inter-Bold;
-  line-height: 24px;
-  color: #000000;
-  text-align: justify;
-}
-.feed-media{
-  padding: 5px;
-  height: 180px;
-  background: rgba(228,228,228, 0.2);
-}
-.feed-media-audio{
-  height: 62px;
-}
-.empty{
-  width: 400px;
-  height: 630px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-}
-.empty-img{
-  width: 285px;
-  height: 190px;
-  margin-bottom: 21px;
-}
-.empty-tip{
-  font-size: 14px;
-  font-family: Inter-Bold;
-  line-height: 24px;
-  color: #000000;
-  margin-top: 20px;
-}
-.more-feed{
-  font-size: 14px;
-  font-family: Inter-Bold;
-  line-height: 24px;
-  color: #FFFFFF;
-}
-.more-icon{
-  width: 22px;
-  height: 18.33px;
-  margin-left: 10px;
-}
+@import url(../../assets/css/teacherfeed.css);
 </style>
