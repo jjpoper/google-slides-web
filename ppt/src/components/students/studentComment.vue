43105f5417356e06b1eb192f78617c21877fc831
kerwinliu <991920040@qq.com>
2022-01-07 19:43:33 +0800

    feedback

--- ppt/src/components/students/studentComment.vue
+++ ppt/src/components/students/studentComment.vue
@@ -1,92 +1,94 @@
 <template>
-  <div class="studentComment" :style="`height: ${webHeight}px; `" v-if="modalVisibale">
+  <div v-if="modalVisibale" class="studentComment">
     <div class="title">
       <p>Teacher Feedback</p>
       <i @click="hidecomment"></i>
     </div>
-    <template v-for="(item, index) in commentList">
-      <div class="feeditem" v-if="item.title" :key="index.toString()">
-        <p class="itemtile">slide {{getIndex(item.pageId)}}</p>
-        <div :class="`readed ${item.id && unreadStudentCommentIds.indexOf(item.id) > -1 ? 'unreadborder' : ''}`">
-          <div class="feedinner">
-            <div class="rightcontent">
-              <div v-show="!slidesVisiable[index]">
-                <div class="right-answer" v-if="getIndexOf(item.title, 'data:image/') > -1">
-                  <div class="pptimage">
-                    <base64image :url="item.title" />
-                  </div>
-                </div>
-                <div class="right-answer" v-else-if="getIndexOf(item.title, '.mp3') > -1">
-                  <div class="pptimage">
-                    <div style="width:80%;">
-                      <audio-player :url="item.title" />
+    <div class="studentCommentList" :style="`height: ${webHeight}px; `" >
+      <template v-for="(item, index) in commentList">
+        <div class="feeditem" v-if="item.title" :key="index.toString()">
+          <p class="itemtile">slide {{getIndex(item.pageId)}}</p>
+          <div :class="`readed ${item.id && unreadStudentCommentIds.indexOf(item.id) > -1 ? 'unreadborder' : ''}`">
+            <div class="feedinner">
+              <div class="rightcontent">
+                <div v-show="!slidesVisiable[index]">
+                  <div class="right-answer" v-if="getIndexOf(item.title, 'data:image/') > -1">
+                    <div class="pptimage">
+                      <base64image :url="item.title" />
                     </div>
                   </div>
+                  <div class="right-answer" v-else-if="getIndexOf(item.title, '.mp3') > -1">
+                    <div class="pptimage">
+                      <div style="width:80%;">
+                        <audio-player :url="item.title" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="right-answer" v-else-if="getIndexOf(item.title, '.webm') > -1">
+                    <div class="pptimage">
+                      <VideoPlayer
+                        controlslist="nodownload"
+                        controls
+                        preload="meta"
+                        :src="item.title"
+                        style="width:80%;"
+                      />
+                    </div>
+                  </div>
+                  <div class="right-answer" v-else-if="getIndexOf(item.title, '[') > -1">
+                    <p v-for="(text,index) in JSON.parse(item.title)" :key="index">{{text}}</p>
+                  </div>
+                  <div class="right-answer" v-else-if="getIndexOf(item.title) === 'file'">
+                    <dash-right-remark-item :item='{content: item.title}' />
+                  </div>
+                  <div class="right-answer" v-else>{{item.title}}</div>
                 </div>
-                <div class="right-answer" v-else-if="getIndexOf(item.title, '.webm') > -1">
-                  <div class="pptimage">
-                    <VideoPlayer
-                      controlslist="nodownload"
-                      controls
-                      preload="meta"
-                      :src="item.title"
-                      style="width:80%;"
-                    />
+                <template v-if="getUrl(item.pageId)">
+                  <div class="right-answer pptitemouter" v-show="slidesVisiable[index]">
+                    <div class="pptitem">
+                      <div class="pptimage" :style="`background-image:url(${getUrl(item.pageId)})`"></div>
+                    </div>
+                    <div class="stpage">{{getIndex(item.pageId)}} / {{slides.length}}</div>
+                  </div>
+                </template>
+                <div class="rightbutton" @click="setVis(index)" v-if="getButtonVis(item.pageId)"></div>
+              </div>
+              <div class="border-line"></div>
+              <div class="rightcomment">
+                <div class="puserinfo">
+                  <p class="uname">{{item.teacherName}}</p>
+                  <p class="utime">{{item.time}}</p>
+                  <i class="uicon">{{item.teacherName.split("")[0]}}</i>
+                </div>
+                <div
+                  class="rightcommentmediadetail"
+                  v-if="item.commentType === 'video' || item.commentType === 'audio'"
+                >
+                  <VideoPlayer
+                    v-if="item.commentType === 'video'"
+                    preload="meta"
+                    controlslist="nodownload"
+                    controls
+                    :src="item.value"
+                    style="width:80%;"
+                  />
+                  <div v-else-if="item.commentType === 'audio'" style="width:80%;">
+                    <audio-player :url="item.title" />
                   </div>
                 </div>
-                <div class="right-answer" v-else-if="getIndexOf(item.title, '[') > -1">
-                  <p v-for="(text,index) in JSON.parse(item.title)" :key="index">{{text}}</p>
-                </div>
-                <div class="right-answer" v-else-if="getIndexOf(item.title) === 'file'">
-                  <dash-right-remark-item :item='{content: item.title}' />
-                </div>
-                <div class="right-answer" v-else>{{item.title}}</div>
+                <div class="rightcommenttextdetail" v-else>{{ item.value }}</div>
+                <!-- <div class="rightcommenttextdetail" >{{ item.value }}</div> -->
               </div>
-              <template v-if="getUrl(item.pageId)">
-                <div class="right-answer pptitemouter" v-show="slidesVisiable[index]">
-                  <div class="pptitem">
-                    <div class="pptimage" :style="`background-image:url(${getUrl(item.pageId)})`"></div>
-                  </div>
-                  <div class="stpage">{{getIndex(item.pageId)}} / {{slides.length}}</div>
-                </div>
-              </template>
-              <div class="rightbutton" @click="setVis(index)" v-if="getButtonVis(item.pageId)"></div>
             </div>
-            <div class="border-line"></div>
-            <div class="rightcomment">
-              <div class="puserinfo">
-                <p class="uname">{{item.teacherName}}</p>
-                <p class="utime">{{item.time}}</p>
-                <i class="uicon">{{item.teacherName.split("")[0]}}</i>
-              </div>
-              <div
-                class="rightcommentmediadetail"
-                v-if="item.commentType === 'video' || item.commentType === 'audio'"
-              >
-                <VideoPlayer
-                  v-if="item.commentType === 'video'"
-                  preload="meta"
-                  controlslist="nodownload"
-                  controls
-                  :src="item.value"
-                  style="width:80%;"
-                />
-                <div v-else-if="item.commentType === 'audio'" style="width:80%;">
-                  <audio-player :url="item.title" />
-                </div>
-              </div>
-              <div class="rightcommenttextdetail" v-else>{{ item.value }}</div>
-              <!-- <div class="rightcommenttextdetail" >{{ item.value }}</div> -->
-            </div>
+            <div
+              v-if="item.id && unreadStudentCommentIds.indexOf(item.id) > -1"
+              class="unread"
+              @click="enterRead(item.id)"
+            ></div>
           </div>
-          <div
-            v-if="item.id && unreadStudentCommentIds.indexOf(item.id) > -1"
-            class="unread"
-            @click="enterRead(item.id)"
-          ></div>
         </div>
-      </div>
-    </template>
+      </template>
+    </div>
   </div>
 </template>
 <script>
@@ -113,6 +115,19 @@ export default {
       currentPageId: 'student/currentPageId',
       currentFeedList: 'student/currentFeedList',
     }),
+    commentList() {
+      const list = [].concat(this.currentFeedList);
+      list.sort((a, b) => {
+        return a.id - b.id
+      })
+      list.sort((a) => {
+        if(a.page_id === this.currentPageId){
+          return -1
+        }
+        return 1
+      })
+      return list
+    }
   },
   props: {
     currentIndex: {
@@ -130,16 +145,10 @@ export default {
       default: () => null
     }
   },
-  watch: {
-    currentFeedList() {
-      console.log(this.currentFeedList, 'currentFeedList')
-      this.refreshList()
-    }
-  },
   data() {
     return {
       modalVisibale: false,
-      webHeight: window.winHeight - 50,
+      webHeight: window.winHeight - 80,
       slidesVisiable: [],
       unreadIdList: []
     };
@@ -176,8 +185,8 @@ export default {
       // const list = getStudentCommentList();
       // this.unreadIdList = getUnreadStudentCommentIds();
       // console.log(list)
-      this.commentList = [].concat(this.currentFeedList);
-      // console.log(list, this.unreadIdList);
+      // this.commentList = [].concat(this.currentFeedList);
+      // console.log(this.commentList);
     },
     getIndex(page_id) {
       const index = this.slides.findIndex(item => item.page_id === page_id);
@@ -222,7 +231,6 @@ export default {
 </script>
 <style scoped>
 .studentComment {
-  overflow-y: scroll;
   box-sizing: border-box;
   padding: 29px 32px 29px 29px;
   background-color: #fff;
@@ -234,6 +242,10 @@ export default {
   right: 0;
   width: 550px;
   box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
+}
+.studentCommentList{
+  overflow-y: scroll;
+  width: 100%;
 }
 .title {
   display: flex;
